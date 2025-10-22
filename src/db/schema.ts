a92db236943c9504e165cbb227d58402284bf9b2
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-10-22 21:15:25 +0200

    add session schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,6 +2,7 @@ import { sqliteTable, text } from "drizzle-orm/sqlite-core";
 import { customUint8Array } from "./types/customUint8Array";
 import { customIsoDate } from "./types/customIsoDate";
 
+// USER TABLE
 export const UsersTable = sqliteTable("users", {
     id: text("id").primaryKey(),
     email: text().notNull(),
@@ -13,3 +14,16 @@ export const UsersTable = sqliteTable("users", {
 });
 export type UserDB = typeof UsersTable.$inferSelect;
 export type UserFrontend = Omit<UserDB, "passwordHash" | "passwordSalt">;
+
+// SESSION TABLE
+export const SessionsTable = sqliteTable("sessions", {
+    id: text("id").primaryKey(),
+    secretHash: customUint8Array().notNull(),
+    createdAt: customIsoDate().notNull(),
+    lastVerifiedAt: customIsoDate().notNull(),
+    // extra props
+    userId: text("user_id").notNull(),
+});
+
+export type SessionDB = typeof SessionsTable.$inferSelect;
+export type SessionFrontend = Omit<SessionDB, "secretHash">;
