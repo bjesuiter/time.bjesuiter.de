c6edf484716b27f399072755269075bc2df927fc
Benjamin Jesuiter <bjesuiter@gmail.com>
2025-11-03 07:56:44 +0100

    add betterAuth Schemas to drizzle db client

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,14 @@
 import { drizzle } from "drizzle-orm/bun-sql";
 import { SQL } from "bun";
 import { envStore } from "@/lib/env/envStore";
+import { betterAuthSchemas } from "./schema/better-auth";
 
 const client = new SQL(envStore.DATABASE_URL);
 
 // You can specify any property from the bun sql connection options
 export const db = drizzle({
     client,
+    schema: {
+        ...betterAuthSchemas,
+    },
 });
--- src/db/schema/better-auth.ts
+++ src/db/schema/better-auth.ts
@@ -1,4 +1,4 @@
-import { sqliteTable, text, integer } from "drizzle-orm/sqlite-core";
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const user = sqliteTable("user", {
   id: text("id").primaryKey(),
@@ -73,3 +73,10 @@ export const verification = sqliteTable("verification", {
     .$onUpdate(() => new Date())
     .notNull(),
 });
+
+export const betterAuthSchemas = {
+  user,
+  session,
+  account,
+  verification,
+};
